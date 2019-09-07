f9ce6e8b866e050d616e289b5a170921329ac5a5
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-09-07 23:21:33 +0700

    lab BCDto7Segment 7493

---
+++ BCDto7Segment/pa.fromHdl.tcl
@@ -0,0 +1,13 @@
+
+# PlanAhead Launch Script for Pre-Synthesis Floorplanning, created by Project Navigator
+
+create_project -name BCDto7Segment -dir "D:/project/CE_Digital-System/BCDto7Segment/planAhead_run_4" -part xc6slx9tqg144-3
+set_param project.pinAheadLayout yes
+set srcset [get_property srcset [current_run -impl]]
+set_property target_constrs_file "BCDto7Segment.ucf" [current_fileset -constrset]
+set hdlfile [add_files [list {BCDto7Segment.vf}]]
+set_property file_type Verilog $hdlfile
+set_property library work $hdlfile
+set_property top BCDto7Segment $srcset
+add_files [list {BCDto7Segment.ucf}] -fileset [get_property constrset [current_run]]
+open_rtl_design -part xc6slx9tqg144-3
