f4d660d5c9d9408155d3e49d52f266d08b00323f
Phumphathai Chansriwong <ouoam2555@gmail.com>
2019-09-15 16:54:49 +0700

    lab 5 finish counter

--- Adder/pa.fromHdl.tcl
+++ Adder/pa.fromHdl.tcl
@@ -1,13 +1,13 @@
 
 # PlanAhead Launch Script for Pre-Synthesis Floorplanning, created by Project Navigator
 
-create_project -name Adder -dir "C:/Users/ouoam/Documents/ISE/Adder/planAhead_run_2" -part xc6slx9tqg144-3
+create_project -name Adder -dir "D:/project/CE_Digital-System/Adder/planAhead_run_5" -part xc6slx9tqg144-3
 set_param project.pinAheadLayout yes
 set srcset [get_property srcset [current_run -impl]]
 set_property target_constrs_file "HalfAdderPin.ucf" [current_fileset -constrset]
-set hdlfile [add_files [list {HalfAdder.vf}]]
+set hdlfile [add_files [list {Adder.vf}]]
 set_property file_type Verilog $hdlfile
 set_property library work $hdlfile
-set_property top HalfAdder $srcset
+set_property top Adder $srcset
 add_files [list {HalfAdderPin.ucf}] -fileset [get_property constrset [current_run]]
 open_rtl_design -part xc6slx9tqg144-3
